7ef4384e50c30aa3cc3ff22dd242d679557d0df4
Gourav Garg <gourav.garg@allegion.com>
2018-06-23 17:51:41 +0530

    Initial Draft

---
+++ Omnify/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Omnify
+//
+//  Created by Gourav  Garg on 23/06/18.
+//  Copyright © 2018 Gourav  Garg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
